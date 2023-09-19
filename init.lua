201ed06a04c983c6ad122ed6d2ca27805efe0aa5
Nathaniel Richardson <31627786+nuhtan@users.noreply.github.com>
2023-09-19 14:23:32 -0400

    Removed extra line

--- init.lua
+++ init.lua
@@ -285,9 +285,6 @@ vim.keymap.set({ 'n', 'v' }, '<Space>', '<Nop>', { silent = true })
 vim.keymap.set('n', 'k', "v:count == 0 ? 'gk' : 'k'", { expr = true, silent = true })
 vim.keymap.set('n', 'j', "v:count == 0 ? 'gj' : 'j'", { expr = true, silent = true })
 
--- File Tree
-vim.keymap.set('n', '<leader>efl', 'Treesitter filesystem reveal left', { silent = true })
-
 -- [[ Highlight on yank ]]
 -- See `:help vim.highlight.on_yank()`
 local highlight_group = vim.api.nvim_create_augroup('YankHighlight', { clear = true })
