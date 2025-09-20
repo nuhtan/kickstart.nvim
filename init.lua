213c7ae34c2bc209f880ef57d9f65a56e005106f
Nathaniel Richardson <ncrichardson15@gmail.com>
2025-09-20 01:26:03 -0400

    additions

--- init.lua
+++ init.lua
@@ -337,6 +337,7 @@ require('lazy').setup({
       -- Enable Telescope extensions if they are installed
       pcall(require('telescope').load_extension, 'fzf')
       pcall(require('telescope').load_extension, 'ui-select')
+      pcall(require('telescope').load_extension, 'project')
 
       -- See `:help telescope.builtin`
       local builtin = require 'telescope.builtin'
@@ -791,17 +792,17 @@ require('lazy').setup({
   },
 
   {
-    'scottmckendry/cyberdream.nvim',
+    'EdenEast/nightfox.nvim',
     priority = 1000,
     config = function()
       ---@diagnostic disable-next-line: missing-fields
-      require('cyberdream').setup {
+      require('nightfox').setup {
         styles = {
           comments = { italic = false }, -- Disable italics in comments
         },
       }
 
-      vim.cmd.colorscheme 'cyberdream'
+      vim.cmd.colorscheme 'terafox'
     end,
   },
 
@@ -891,7 +892,7 @@ require('lazy').setup({
   --    This is the easiest way to modularize your config.
   --
   --  Uncomment the following line and add your plugins to `lua/custom/plugins/*.lua` to get going.
-  -- { import = 'custom.plugins' },
+  { import = 'custom.plugins' },
   --
   -- For additional information with loading, sourcing and examples see `:help lazy.nvim-🔌-plugin-spec`
   -- Or use telescope!
--- lua/custom/plugins/init.lua
+++ lua/custom/plugins/init.lua
@@ -1,5 +1,32 @@
--- You can add your own plugins here or in other files in this directory!
---  I promise not to create any merge conflicts in this directory :)
---
--- See the kickstart.nvim README for more information
-return {}
+return {
+    {
+        "folke/todo-comments.nvim",
+        dependencies = { "nvim-lua/plenary.nvim" },
+    },
+    {
+        "folke/trouble.nvim",
+        dependencies = { "nvim-tree/nvim-web-devicons" },
+    },
+    {
+        'hedyhli/outline.nvim',
+        lazy = true,
+        keys = {
+            { '\\/', ':Outline<CR>', desc = 'Outline', silent = true },
+
+        },
+        opts = {
+            position='right',
+        }
+    },
+    {
+        'nvim-mini/mini.files',
+        version = false,
+        config = function()
+           require('mini.files').setup() 
+        end
+    },
+    {
+        'nvim-telescope/telescope-project.nvim',
+        dependencies = {'nvim-telescope/telescope.nvim'},
+    }
+}
