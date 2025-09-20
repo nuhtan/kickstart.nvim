de8868a538ed90882f4c290df4113e2503cda388
Nathaniel Richardson <ncrichardson15@gmail.com>
2025-09-20 01:50:46 -0400

    Added keybinds for new plugins

--- lua/custom/plugins/init.lua
+++ lua/custom/plugins/init.lua
@@ -1,32 +1,37 @@
 return {
-    {
-        "folke/todo-comments.nvim",
-        dependencies = { "nvim-lua/plenary.nvim" },
+  {
+    'folke/todo-comments.nvim',
+    dependencies = { 'nvim-lua/plenary.nvim' },
+  },
+  {
+    'folke/trouble.nvim',
+    dependencies = { 'nvim-tree/nvim-web-devicons' },
+  },
+  {
+    'hedyhli/outline.nvim',
+    lazy = true,
+    keys = {
+      { ' eo', ':Outline<CR>', desc = 'Outline', silent = true },
     },
-    {
-        "folke/trouble.nvim",
-        dependencies = { "nvim-tree/nvim-web-devicons" },
+    opts = {
+      position = 'right',
     },
-    {
-        'hedyhli/outline.nvim',
-        lazy = true,
-        keys = {
-            { '\\/', ':Outline<CR>', desc = 'Outline', silent = true },
-
-        },
-        opts = {
-            position='right',
-        }
+  },
+  {
+    'nvim-mini/mini.files',
+    version = false,
+    keys = {
+      { ' ef', ':lua MiniFiles.open()<CR>', desc = 'File Selector', silent = true },
     },
-    {
-        'nvim-mini/mini.files',
-        version = false,
-        config = function()
-           require('mini.files').setup() 
-        end
+    config = function()
+      require('mini.files').setup()
+    end,
+  },
+  {
+    'nvim-telescope/telescope-project.nvim',
+    dependencies = { 'nvim-telescope/telescope.nvim' },
+    keys = {
+      { ' ep', ":lua require('telescope').extensions.project.project{}<CR>", desc = 'Project Selector', silent = true },
     },
-    {
-        'nvim-telescope/telescope-project.nvim',
-        dependencies = {'nvim-telescope/telescope.nvim'},
-    }
+  },
 }
